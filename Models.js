cf0a4b1539c43acdf4d34c2b76573a6a775f9d5d
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-03-20 21:18:02 -0400

    login logout hotel booking done

---
+++ Models.js
@@ -0,0 +1,26 @@
+class HotelBooking {
+  constructor(name,room,adults,children,checkindate,checkoutdate,userId) {
+    this.name = name;
+    this.room = room;
+     this.adults = adults;
+     this.children = children;
+     this.checkIn = checkindate;
+     this.checkOut = checkoutdate;
+     this.userID = userId;
+  }
+}
+class  User {
+  constructor(fname,lname,password,email,username) {
+    this.fname = fname;
+    this.lname = lname;
+    this.password = password;
+    this.email = email;
+    this.username = username;
+  }
+}
+class FlightBooking{
+  constructor(flyingFrom, flyingTo, departing, returning,adults , children, travelClass){
+
+
+  }
+}
